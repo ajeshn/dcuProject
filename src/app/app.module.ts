bf41ee90bbe74bbe4963db0d90167fb0e2276189
ajeshn <kandavathajesh@gmail.com>
2023-03-14 20:11:16 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DcuComponent } from './dcu/dcu.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DcuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
